cbd120aa5af089c97e3ecb5e2e40460822a8325f
Andung <andung@Andungs-MacBook-Pro.local>
2020-01-22 06:34:48 +0800

    first commit

---
+++ phpmvc.sql
@@ -0,0 +1,14 @@
+create database phpmvc;
+use phpmvc;
+
+
+create table mahasiswa(
+id int auto_increment,
+stambuk varchar(250),
+nama varchar(250),
+email varchar(250),
+jurusan varchar(250),
+primary key (id) 
+
+);
+
